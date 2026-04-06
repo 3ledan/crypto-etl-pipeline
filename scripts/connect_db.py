e5051f16527623d3043ee19bf8dc84db3aa166fb
John Eledan <167026993+3ledan@users.noreply.github.com>
2026-04-06 10:05:56 +0100

    Update insert_data function to include total_volume

--- scripts/connect_db.py
+++ scripts/connect_db.py
@@ -1,13 +1,14 @@
-import psycopg2
 from config import DB_CONFIG
+import psycopg2
 
-# Establish Python-PostgresSQL Connection
-def insert_data(currency, price_usd, market_cap, change_24h):
+def insert_data(currency, price_usd, market_cap, total_volume, price_change_24h):
     conn = psycopg2.connect(**DB_CONFIG)
     cur = conn.cursor()
-    insert_query = "INSERT INTO crypto_prices (currency, price_usd, market_cap, change_24h) VALUES (%s,%s,%s,%s)"
-    cur.execute(insert_query, (currency, price_usd, market_cap, change_24h))
 
+    # Replace "table_name" with your table name
+    insert_query = "INSERT INTO table_name (currency, price_usd, market_cap, total_volume, price_change_24h) VALUES (%s,%s,%s,%s,%s)"
+    cur.execute(insert_query, (currency, price_usd, market_cap, total_volume, price_change_24h))
+    
     conn.commit()
     cur.close()
     conn.close()
