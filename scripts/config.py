e3dbf156f401f237ed7fbb1b0351c9fe1227dcc3
John Eledan <167026993+3ledan@users.noreply.github.com>
2026-04-06 10:04:14 +0100

    Update API_URL to include pagination parameters

--- scripts/config.py
+++ scripts/config.py
@@ -8,4 +8,4 @@ DB_CONFIG = {
 }
 
 # API URL
-API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum,solana"
+API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1"
